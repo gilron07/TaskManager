2e7727c06a261447fdd88aa754f6d5f5da684f27
Gilad Leifman <leifmangilad@gmail.com>
2019-07-08 23:35:50 +0300

    Format 2 documents

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,15 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // require('../server/db');
 
-
-
-var users = mongoose.model('users');
+var users = mongoose.model("users");
 
 /* GET users listing. */
-router.get('/a', (req, res) => {
-  users.find().then((result)=> {
+router.get("/a", (req, res) => {
+  users.find().then(result => {
     res.send(result);
   });
 });
